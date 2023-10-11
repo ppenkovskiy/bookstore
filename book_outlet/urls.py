adc5c2599819090bc7c613a47e2cdc1a3a47fb1f
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-11 18:36:30 +0300

    c9

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('<int:id>', views.book_detail, name='book-detail')
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.http import Http404
 
 
 def index(request):
@@ -7,3 +8,19 @@ def index(request):
     return render(request, 'index.html',
                   {'books': books}
                   )
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    # the same below in 1 line of code
+    book = get_object_or_404(Book, pk=id)
+
+    return render(request, 'book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestseller': book.is_bestselling
+    })
